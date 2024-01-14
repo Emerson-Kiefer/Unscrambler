811e91c8416801591e9d145e06f30e2e77cfb92d
Emerson-Kiefer <emerson.kiefer01@gmail.com>
2024-01-14 16:48:24 -0500

    Create image capture functions

--- CameraManager.py
+++ CameraManager.py
@@ -2,36 +2,31 @@ import cv2
 
 CAM_0_INDEX = 0
 CAM_1_INDEX = 2
+CAM_INDEXES = [CAM_0_INDEX, CAM_1_INDEX]
+IMAGE_OUTPUT_PATH = '/home/unscrambler/images/'
 
-cam0 = cv2.VideoCapture(CAM_0_INDEX)
+def capture_image(cam_index, cam_num):
+	cam = cv2.VideoCapture(cam_index)
+	
+	if not cam.isOpened():
+		print("Cannot open camera", i)
+		return
+	
+	while True:
+		ret, image = cam.read()
+		img_name = "test_image" + str(cam_num)
+		cv2.imshow(img_name, image)
+		k = cv2.waitKey(1)
+		if k != -1:
+			break
+	cv2.imwrite(IMAGE_OUTPUT_PATH + img_name + ".jpg", image)
+	cam.release()
 
-if not cam0.isOpened():
-	print("Cannot open camera")
-	exit()
-while True:
-	ret0, image0 = cam0.read()
-	cv2.imshow('Image0',image0)
-	k= cv2.waitKey(1)
-	if k != -1:
-		break
+def capture_images(cam_indexes):
+	for cam_num, cam_index in enumerate(cam_indexes):
+		print("Camera", cam_num)
+		capture_image(cam_index, cam_num)
+	cv2.destroyAllWindows()
 
-cv2.imwrite('/home/unscrambler/images/test0.jpg', image0)
-cam0.release()
+capture_images(CAM_INDEXES)
 
-
-
-cam1 = cv2.VideoCapture(CAM_1_INDEX)
-
-if not cam1.isOpened():
-	print("Cannot open camera")
-	exit()
-while True:
-	ret1, image1 = cam1.read()
-	cv2.imshow('Image1',image1)
-	k= cv2.waitKey(1)
-	if k != -1:
-		break
-
-cv2.imwrite('/home/unscrambler/images/test1.jpg', image1)
-cam1.release()
-cv2.destroyAllWindows()
